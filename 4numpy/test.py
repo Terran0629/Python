070aa321a46d5f0c40e742f8599b113944fa16aa
Terran0629 <154040415+Terran0629@users.noreply.github.com>
2024-01-27 21:33:08 +0800

    Add files via upload

---
+++ 4numpy/test.py
@@ -0,0 +1,3 @@
+def f():
+    li1 = list(range(10000))
+    li2 = list(range(10000))    
---
+++ 4numpy/writefile_test.py
@@ -0,0 +1,8 @@
+print('hello')
+print('writefile')
+'''
+writefile将单元格内容写入到文件中。如果文件不存在则创建,
+如果文件存在,则覆盖文件。如果指定-a参数,则追加内容,不覆盖。
+'''
+print('append....')
+m = 666
